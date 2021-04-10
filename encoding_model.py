6e9d5e83a5b0d110083bf3812349cd5b8e012f62
Russell Poldrack <poldrack@gmail.com>
2021-04-10 11:55:34 -0700

    add alphas

--- encoding_model.py
+++ encoding_model.py
@@ -4,19 +4,25 @@ using sklearn interface
 """
 
 from sklearn.linear_model import LinearRegression, RidgeCV
+import numpy as np
 
 
 class EncodingModel:
-    def __init__(self, method='ridgecv', cv=None):
+    def __init__(self, method='ridgecv', cv=None, alphas=None):
         self.method = method
         self.cv = cv  # default uses efficient gcv
         self.clf = None
+        if alphas is not None:
+            self.alphas = alphas
+        else:
+            self.alphas = [0.001, 0.01, 0.1]
+            self.alphas.extend(np.linspace(1, 10, 10))
 
     def fit(self, X, y):
         if self.method == 'lr':
             self.clf = LinearRegression()
         elif self.method == 'ridgecv':
-            self.clf = RidgeCV(cv=self.cv)
+            self.clf = RidgeCV(cv=self.cv, alphas=self.alphas)
         else:
             raise Exception(f'method {self.method} not implemented')
 
